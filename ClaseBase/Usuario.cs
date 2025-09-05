c8186de6702019aff53106d0acb46bc8f76fe130
ArielS <diciembre93@gmail.com>
2025-09-05 18:13:30 -0300

    se crearon 4 clases

---
+++ ClaseBase/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseBase
+{
+    public class Usuario : Roles
+    {
+        public int Usu_ID { get; set; }
+        public string Usu_NombreUsuario { get; set; }
+        public string Usu_Contraseña { get; set; }
+        public string Usu_ApellidoNombre { get; set; }
+        public int Rol_ID { get; set; }
+    }
+}
