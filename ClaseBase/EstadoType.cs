79baacc85fb36cfe955624d3b27d671dc8aef8ba
pablo-bejarano2 <pablobejarano2112@gmail.com>
2025-09-06 11:40:28 -0300

    add clases al diagrama de clases

---
+++ ClaseBase/EstadoType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseBase
+{
+    public class EstadoType
+    {
+        public int Esty_ID { get; set; }
+        public string Esty_Nombre { get; set; } //Tiene que ser Curso o Inscripcion
+    }
+}
